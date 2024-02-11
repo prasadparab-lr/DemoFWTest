29eef82e9db0adbc3cb390101469da9834bc209e
prasadparab-lr <142771777+prasadparab-lr@users.noreply.github.com>
2024-02-11 20:43:20 +0530

    updated post clone script

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -3,5 +3,17 @@
 # Install CocoaPods using Homebrew.
 brew install cocoapods
 
+
+## create the `netrc` file
+touch ~/.netrc
+
+## write the machine name into the `netrc file
+echo "machine github.com" > ~/.netrc
+## write the username into the `netrc file
+echo "login elitesdk" >> ~/.netrc 
+## write the password, obtained from an environment variable, into the `netrc file
+echo "password ${SDK_TOKEN}" >> ~/.netrc
+
+
 # Install dependencies you manage with CocoaPods.
 pod install
