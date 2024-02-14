15ee80db0ea029d0e035529f23bb284920a7580f
prasadparab-lr <142771777+prasadparab-lr@users.noreply.github.com>
2024-02-14 22:39:55 +0530

    added new pod

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -8,11 +8,14 @@ brew install cocoapods
 touch ~/.netrc
 
 ## write the machine name into the `netrc file
-echo "machine github.com" > ~/.netrc
+echo "machine github.com login elitesdk password  ${SDK_TOKEN}" > ~/.netrc
 ## write the username into the `netrc file
-echo "login elitesdk" >> ~/.netrc 
+#echo "login elitesdk" >> ~/.netrc 
 ## write the password, obtained from an environment variables, into the `netrc file
-echo "password ${SDK_TOKEN}" >> ~/.netrc
+#echo "password ${SDK_TOKEN}" >> ~/.netrc
+
+
+echo "machine github.com login prapa1190 password  ${NEW_TOKEN}" > ~/.netrc
 
 
 chmod 600 ~/.netrc
